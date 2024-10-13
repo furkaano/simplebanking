b71022415eab4f16ade8f6e96b50f4b3aefb39f5
Furkan Öcalan <furkan1907903@gmail.com>
2024-10-13 16:51:05 +0300

    Unnecessary comments removed

--- src/test/java/com/eteration/simplebanking/ControllerTests.java
+++ src/test/java/com/eteration/simplebanking/ControllerTests.java
@@ -39,20 +39,7 @@ class ControllerTests  {
     public void setup() {
         objectMapper = new ObjectMapper();
     }
-/*
-    // Test 1
-    @Test
-    public void givenId_Credit_thenReturnJson()
-    throws Exception {
-        
-        Account account = new Account("Kerem Karaca", "17892");
 
-        doReturn(account).when(service).findAccount( "17892");
-        ResponseEntity<TransactionStatus> result = controller.credit( "17892", new DepositTransaction(1000.0));
-        verify(service, times(1)).findAccount("17892");
-        assertEquals("OK", result.getBody().getStatus());
-    }
-*/
     // Replacement of Test 1
     @Test
     public void shouldReturnSuccessStatusWhenCreditTransactionIsProcessed() throws Exception {
@@ -78,24 +65,6 @@ class ControllerTests  {
         // Verify that the service was called correctly
         verify(accountService, times(1)).processTransaction(eq(accountNumber), any(DepositTransaction.class));
     }
-
-/*
-    // Test 2
-    @Test
-    public void givenId_CreditAndThenDebit_thenReturnJson()
-    throws Exception {
-        
-        Account account = new Account("Kerem Karaca", "17892");
-
-        doReturn(account).when(service).findAccount( "17892");
-        ResponseEntity<TransactionStatus> result = controller.credit( "17892", new DepositTransaction(1000.0));
-        ResponseEntity<TransactionStatus> result2 = controller.debit( "17892", new WithdrawalTransaction(50.0));
-        verify(service, times(2)).findAccount("17892");
-        assertEquals("OK", result.getBody().getStatus());
-        assertEquals("OK", result2.getBody().getStatus());
-        assertEquals(950.0, account.getBalance(),0.001);
-    }
-*/
 
     // Replacement of Test 2
     @Test
@@ -123,24 +92,6 @@ class ControllerTests  {
         verify(accountService, times(1)).processTransaction(eq(accountNumber), any(WithdrawalTransaction.class));
     }
 
-    /*
-        // Test 3
-        @Test
-        public void givenId_CreditAndThenDebitMoreGetException_thenReturnJson()
-        throws Exception {
-            Assertions.assertThrows( InsufficientBalanceException.class, () -> {
-                Account account = new Account("Kerem Karaca", "17892");
-
-                doReturn(account).when(service).findAccount( "17892");
-                ResponseEntity<TransactionStatus> result = controller.credit( "17892", new DepositTransaction(1000.0));
-                assertEquals("OK", result.getBody().getStatus());
-                assertEquals(1000.0, account.getBalance(),0.001);
-                verify(service, times(1)).findAccount("17892");
-
-                ResponseEntity<TransactionStatus> result2 = controller.debit( "17892", new WithdrawalTransaction(5000.0));
-            });
-        }
-    */
     // Replacement of Test 3
     @Test
     public void shouldReturnFailStatusWhenDebitTransactionExceedsBalance() throws Exception {
@@ -166,20 +117,6 @@ class ControllerTests  {
         verify(accountService, times(1)).processTransaction(eq(accountNumber), any(WithdrawalTransaction.class));
     }
 
-    /*
-        //Test 4
-        @Test
-        public void givenId_GetAccount_thenReturnJson()
-        throws Exception {
-
-            Account account = new Account("Kerem Karaca", "17892");
-
-            doReturn(account).when(service).findAccount( "17892");
-            ResponseEntity<Account> result = controller.getAccount( "17892");
-            verify(service, times(1)).findAccount("17892");
-            assertEquals(account, result.getBody());
-        }
-    */
     // Replacement of Test 4
     @Test
     public void shouldReturnAccountDetailsWhenAccountExists() throws Exception {
